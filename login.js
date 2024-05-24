73545e48ab2705bbb0eedc936c1c8f7db0bbf29f
5i2 <5i2@cangallo.escuela>
2024-05-24 09:11:54 -0300

    a

--- login.js
+++ login.js
@@ -1,7 +1,17 @@
 $(document).ready(function()
 {
-    $("#btnlogin").on("click", function() 
-    {
-        alert("hola")
+    $("#btnlogin").on("click", function(){
+        usuario = $("#user").val()
+        contrasena = $("#password").val()
+        if(usuario == ""){
+            alert("El usuario esta vacio")
+        } else{
+            //alert("Ahora deberia pasar algo")
+            if(contrasena == ""){
+                alert("La contraseña está vacía")
+            } else{
+                location = "answer.html"
+            }
+        }
     })
 })
